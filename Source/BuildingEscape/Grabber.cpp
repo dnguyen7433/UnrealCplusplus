035c369cd46ee0ac8549799bdaaeefa5426c38b6
dnguyen7433 <nguyendieuhuyen2809@gmail.com>
2019-01-13 21:37:18 -0600

    BE_Adding grabbing functionality to the pawn and Extending the C++ game mode to BluePrint

---
+++ Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+#include "GameFramework/Actor.h "
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Error, TEXT("Grabber is reporting for duty!"));// *  here is NOT dereferencing the pointer to an memory address, 
+												// but * is an overloaded operator that macro in Unreal used.
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
